033369481413ecb3a2527e0d84ce05e7a3683fba
MdFirojulIslam <169171165+MdFirojulIslam@users.noreply.github.com>
2024-05-20 16:32:16 +0600

    Add files via upload

---
+++ function/home.php
@@ -0,0 +1,70 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ADMIN-panel</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	 <div class="header">
+        <nav class="navbar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Sign In</a></li>
+                <li>
+                    <form action="home.php">
+                        <input type="search" name="search" placeholder="Search...">
+                    </form>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+	<div class="container">
+        <ul>
+            <li><a href="#">View Products</a></li>
+            <li><a href="#">Insert Product</a></li>
+            <li><a href="#">Insert Categories</a></li>
+            <li><a href="#">View Categories</a></li>
+            <li><a href="#">Insert Brands</a></li>
+            <li><a href="#">VIew Brands</a></li>
+            <li><a href="#">All Orders</a></li>
+            <li><a href="#">All Payment</a></li>
+            <li><a href="#">list Users</a></li>
+        </ul>
+    </div>
+
+    <div class="show-details">
+    	<table>
+    		<thead>
+    			<tr>
+    				<th>Product ID</th>
+    			<th>Product Title</th>
+    			<th>Product Image</th>
+    			<th>Product Price</th>
+    			<th>Toatal Sold</th>
+    			<th>Status</th>
+    			<th>Edit</th>
+    			<th>Delete</th>
+    			</tr>
+    			
+    		</thead>
+    		<tbody>
+    			<tr>
+    				<td>sgfvsdsds</td>
+    			<td>sgfvsdsds</td>
+    			<td>sgfvsdsds</td>
+    			<td>sgfvsdsds</td>
+    			<td>sgfvsdsds</td>
+    			<td>sgfvsdsds</td>
+    			<td>sgfvsdsds</td>
+    			<td>sgfvsdsds</td>
+    			</tr>
+    			
+    		</tbody>
+    	</table>
+    </div>
+</body>
+</html>
